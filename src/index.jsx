811019582fc4130a487010735746a795252d9f4c
Karl Keller <kgk86@hotmail.com>
2021-08-06 19:33:02 +0200

    Try rendering nested elements using a React.fragment

--- src/index.jsx
+++ src/index.jsx
@@ -14,5 +14,13 @@ const Hello = ({ name }) => {
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(
+    <>
+      <h1>nested content</h1><br />
+      <h2><Hello name="Karl" /></h2>
+      <h3><Hello name="B" /></h3><br />
+      <p>First paragraph</p>
+      <p><Hello name="World" /></p><br />
+    </>, root
+  );
 }
