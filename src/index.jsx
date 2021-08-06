2c9476f82011f7794a34a5d5c3b1fd9f4ef97fd1
Karl Keller <kgk86@hotmail.com>
2021-08-06 20:43:13 +0200

    Setup project: Create app.jsx and undo Hello components

--- src/index.jsx
+++ src/index.jsx
@@ -2,43 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: true
-    };
-  }
-
-  handleClick = () => {
-    console.log('clicked');
-    this.setState({
-      clicked: !this.state.clicked
-    });
-  }
-
-  render() {
-    // Build and return HTML
-    return (
-      <div className={this.state.clicked ? 'clicked' : null}
-      onClick={this.handleClick}>
-      Hello 
-      {this.props.name}</div>
-    );
-  }
-}
+import App from './components/app';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(
-    <>
-      <h1>nested content</h1><br />
-      <h2><Hello name="Karl" /></h2>
-      <h3><Hello name="B" /></h3><br />
-      <p>First paragraph</p>
-      <p><Hello name="World" /></p><br />
-    </>, root
-  );
+  ReactDOM.render(<App />, root);
 }
