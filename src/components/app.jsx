36fdbebc037045d3c5043bfbb52fa3087a1819b2
Karl Keller <kgk86@hotmail.com>
2021-08-06 22:55:30 +0200

    Add array of GIF ids to test GifList compnent

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import Gif from './gif';
+import GifList from './gif_list';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
   render() {
+    const gifs = [
+      { id: "l10bV0Vt2MiyucTMjZ" },
+      { id: "xT9IgDEI1iZyb2wqo8" }
+    ];
+
     return (
       <div>
-        <dix className="left-scene">
+        <div className="left-scene">
           <SearchBar />
           <dir className="selected-gif">
-            <Gif id="l10bV0Vt2MiyucTMjZ"/>
+            <Gif id="l10bV0Vt2MiyucTMjZ" />
           </dir>
-        </dix>
-        <dix className="right-scene"></dix>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
