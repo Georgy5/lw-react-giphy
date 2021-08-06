e3294b4a7fdf2fdaf8ef2a3f5781de3c0cb0ee76
Karl Keller <kgk86@hotmail.com>
2021-08-06 22:54:17 +0200

    Create GifList Class Component

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/require-render-return */
+import React, { Component } from 'react';
 
-const GifList = () => {
-  return (
-    <>
-    </>
-  );
-};
+import Gif from './gif';
 
-export default GifList;
+// eslint-disable-next-line react/prefer-stateless-function
+export default class GifList extends Component {
+  render() {
+    // eslint-disable-next-line arrow-parens
+    return this.props.gifs.map(gif => {
+      return <Gif id={gif.id} />;
+    });
+  }
+}
