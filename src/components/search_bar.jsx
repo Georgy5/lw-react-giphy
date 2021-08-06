4cc37dc3ca4f2f42ca2285ba28200455088c55d5
Karl Keller <kgk86@hotmail.com>
2021-08-06 23:37:36 +0200

    Add handleUpdate function

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class SearchBar extends Component {
+  handleUpdate = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <input type="text" className="form-search" />
+      <input
+        type="text"
+        className="form-search"
+        onChange={this.handleUpdate}
+      />
     );
   }
 }
