e8106305ada1b28bc4a45a6db3bcddd1ab735fbf
Karl Keller <kgk86@hotmail.com>
2021-08-06 22:25:45 +0200

    Create Gif Class Component and test rendering a gif

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Gif = () => {
-  return (
-    <>
-    </>
-  );
-};
-
-export default Gif;
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Gif extends Component {
+  render() {
+    const src = "https://media.giphy.com/media/l10bV0Vt2MiyucTMjZ/giphy.gif";
+    return (
+      <img src={src} alt="" className="gif" />
+    );
+  }
+}
