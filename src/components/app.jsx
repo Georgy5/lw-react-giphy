6473a52117aff49e8c9b57032876485e441eaf61
Karl Keller <kgk86@hotmail.com>
2021-08-06 21:55:43 +0200

    Create React Class Component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div />
-  );
-};
-
-export default App;
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <dix className="left-scene"></dix>
+        <dix className="right-scene"></dix>
+      </div>
+    );
+  }
+}
